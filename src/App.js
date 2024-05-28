8a9ac7ea9348a1d6f6d62fc809a35b06c648a407
JustinL197 <159653227+JustinL197@users.noreply.github.com>
2024-05-27 23:50:55 -0400

    Add VideoPlayer component - without styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import React from 'react';
 import Header from './components/Header/Header';
-import "./App.scss"
+import VideoPlayer from './components/VideoPlayer/VideoPlayer';
+import videoListJson from './data/videos.json';
+import videoDetailsJson from './data/video-details.json';
+import "./App.scss";
 
 function App() {
+
+  const [videoList, setVideoList] = useState([]);
+  const [currentVideo, setCurrentVideo] = useState(null);
+
+  //function to fetch video list
+  const fetchVideoList = () => {
+    return videoListJson;
+  };
+
+  const fetchVideoDetails = (videoId) => {
+    const video = videoDetailsJson.find((video) => (video.id === videoId));
+    return video;
+  };
+
+  //simulating fetching data from APIs
+  useEffect(() => {
+    const videos = fetchVideoList();
+    setVideoList(videos);
+
+    if (videos.length > 0) {
+      const video = fetchVideoDetails(videos[0].id);
+      setCurrentVideo(video);
+    }
+  }, []);
+
+  const setVideoSelect = (videoId) => {
+    const video = fetchVideoDetails(videoId);
+    setCurrentVideo(video);
+  };
+
   return (
     <div className="App">
       <Header />
+      <main>
+        {currentVideo && <videoPlayer video={currentVideo} />}
+        <VideoPlayer video={video} />
+      </main>
     </div>
   );
 }
---
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './VideoPlayer.scss';
+
+function VideoPlayer({video}){
+    return (
+        <div className="video-player">
+            <video className="video-player__video" controls poster={video.poster}>
+                <source src={video.src} type="video/mp4"/>
+            </video>
+            <div className="video-player__details">
+                <h1 className="video-player__title">{video.title}</h1>
+                <div className="video-player__container">
+                    <div className="video-player__subcontainer">
+                        <p className="video-player__author">{video.author}</p>
+                        <p className="video-player__date">{video.date}</p>
+                    </div>
+                    <div className="video-player__subcontainer">
+                        <p className="video-player__views">{video.views}</p>
+                        <p className="video-player__likes">{video.likes}</p>
+                    </div>
+                    <p className="video-player__description">{video.description}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default VideoPlayer;
